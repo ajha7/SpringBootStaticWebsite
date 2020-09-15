8d73c78299a88b26bab315c9f92371527cd63fb2
Anshul Jha <65700919+ajha7@users.noreply.github.com>
2020-09-15 15:56:21 -0700

    Update LoginController.java

--- src/main/java/com/example/infrastructureForProjects/Controller/LoginController.java
+++ src/main/java/com/example/infrastructureForProjects/Controller/LoginController.java
@@ -26,11 +26,6 @@ public class LoginController {
         return "index";
     }
 
-   /* @PreAuthorize("isAuthenticated()")
-    public String currentUserName() {
-        return userService.findByUsername();
-    } */
-
     @GetMapping(value = "/register")
     public String registerPage(){
         return "register";
@@ -47,16 +42,13 @@ public class LoginController {
     }
 
     @PostMapping(value = "/register")
-    public String addUser(@RequestParam(name = "username") String username, @RequestParam(name = "password") String password, @RequestParam(name = "confirmation") String confirmPassword, /*Errors errors,*/ RedirectAttributes redirectAttributes) {
-        //if(userService.findByUsername(username) != null)
+    public String addUser(@RequestParam(name = "username") String username, @RequestParam(name = "password") String password, @RequestParam(name = "confirmation") String confirmPassword, RedirectAttributes redirectAttributes) {
         if(userService.findUserByUsername(username).isPresent())
         {
-            //errors.rejectValue("usernameExists", "Username already exists. Try again.");
             redirectAttributes.addFlashAttribute("errorMessage", "Username already exists. Try again.");
             return "redirect:/register";
         }
         else if (!password.equals(confirmPassword)) {
-            //errors.rejectValue("passwordFailMatch", "Passwords do not match. Try again.");
             redirectAttributes.addFlashAttribute("errorMessage", "Passwords do not match. Try again.");
             return "redirect:/register";
         }
@@ -72,39 +64,4 @@ public class LoginController {
         model.addAttribute("errorMessage", "The username or password is incorrect. Try again.");
         return "login.html";
     }
-
-  /*  @PostMapping(value = "/doLogin")
-    public String doLogin(@RequestParam(name = "username") String username, @RequestParam(name = "password") String password) {
-        //if(userService.findByUsername(username) != null)
-        if(userService.findUserByUsername(username).isPresent())
-        {
-            //System.out.print("HEREEEE:" + userService.findUserByUsername(username).isPresent());
-            return "/login-error";      //would need to return a failed login
-        }
-        else if (userService.findPassword(password)) {      //note to self: optimize later
-            return "login-error";       //would need to return a failed login
-        }
-        else {
-            return "/";                 //would need to return a successful login
-        }
-    } */
-
-
-    /*@GetMapping("/users")
-    public List<User> allUsers() {
-        return userService.listAll();
-    }
-
-    @GetMapping(path="/users/count")
-    public Long count() {
-
-        return userService.count();
-    }*/
-
-   /* @DeleteMapping(path="/users/{id}")
-    public void delete(@PathVariable String id) {
-
-        Long userId = Long.parseLong(id);
-        userService.deleteById(userId);
-    } */
 }
