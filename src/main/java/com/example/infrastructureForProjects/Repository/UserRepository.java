06b1b00792d83aff4534335fc2089db09585887e
Anshul Jha <65700919+ajha7@users.noreply.github.com>
2020-09-15 15:58:57 -0700

    Update UserRepository.java

--- src/main/java/com/example/infrastructureForProjects/Repository/UserRepository.java
+++ src/main/java/com/example/infrastructureForProjects/Repository/UserRepository.java
@@ -3,23 +3,16 @@ package com.example.infrastructureForProjects.Repository;
 import com.example.infrastructureForProjects.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import javax.swing.text.html.Option;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-    // @Query(value = "SELECT username FROM usersDB WHERE username=:name", nativeQuery = true)
-    // String findUsername(@Param("name") String name);
-
     Optional<User> findByUsername(String username);
 
     @Query(value = "SELECT hashed_password FROM usersDB WHERE hashed_password=:hashedPassword", nativeQuery = true)
     String findHash(@Param("hashedPassword") String hashedPassword);
 
-    //@Query(value = "SELECT hashed_password FROM usersDB WHERE username=:name", nativeQuery = true)
-    // String findHashByUsername(@Param("name") String name);
 }
